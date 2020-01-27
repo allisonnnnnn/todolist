0c044fed009e82cb99d46e135b9938511fa20063
allison <allisonchen1130@gmail.com>
2020-01-26 23:02:52 -0500

    Input,Add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+// TODO:
+// 1. extract out component
+// 2. styling
+// 3. clear input when after adding
+// 4. implement delete item
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      list: []
+    };
+  }
+
+  changeInput = e => {
+    this.setState({ input: e.target.value });
+  };
+
+  addListItem = () => {
+    // list: this.state.list.concat([this.state.input])});
+    const newList = [...this.state.list];
+    newList.push(this.state.input);
+    this.setState({
+      list: newList
+    });
+  };
+
+  render() {
+    const { list, input } = this.state;
+
+    return (
+      <div className="App">
+        <input type="text" onChange={this.changeInput} />
+        <button onClick={this.addListItem}>Add</button>
+        {/* use ul li to create list item */}
+        <ul>
+          {list.map(item => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
